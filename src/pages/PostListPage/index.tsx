37ec184288e1142733df40e02c244e4269ae2cb3
dustmddus <dustmddus5@gmail.com>
2022-09-04 17:32:29 +0900

    feat: 공고 리스트 페이지 api 연동

--- src/pages/PostListPage/index.tsx
+++ src/pages/PostListPage/index.tsx
@@ -1,16 +1,64 @@
 import { FilterList } from "src/components";
 import * as S from "./PostListPage.style";
-import { dummyData } from "./dummyData";
 import PostItem from "src/components/PostItem";
 import { Values } from "./types";
+import { useEffect, useState } from "react";
+import { axiosAuthInstance } from "src/apis/axiosInstances";
+import { useRecoilValue } from "recoil";
+import { sportsCategory } from "src/recoil/category";
+import { userInfo } from "src/recoil/user";
+import { AxiosResponse } from "axios";
+import { Response } from "../PostListPage/types";
 
 const PostListPage = () => {
+  const category = useRecoilValue(sportsCategory);
+  const [state, setState] = useState<Response>({
+    cursor: {
+      createdAt: "",
+      id: null,
+    },
+    values: [],
+    hasNext: false,
+    category: "",
+  });
+  const user = useRecoilValue(userInfo);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        setIsLoading(true);
+        const res = await axiosAuthInstance.get("/api/matches", {
+          params: {
+            size: 10,
+            category,
+            status: "WAITING",
+            userId: "",
+            distance: user.searchDistance,
+          },
+        });
+        const data = (res.data as AxiosResponse).data as Response;
+
+        setState({
+          values: data.values,
+          hasNext: data.hasNext,
+          cursor: data.cursor,
+          category: category,
+        });
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getPost();
+  }, [category]);
+
   return (
     <S.Container>
       <S.Input placeholder="검색어를 입력하세요" />
       <FilterList />
       <S.ListContainer>
-        {dummyData.data.values.map((i: Values) => (
+        {state.values.map((i: Values) => (
           <PostItem
             id={i.id}
             key={i.id}
@@ -21,6 +69,7 @@ const PostListPage = () => {
             matchType={i.matchType}
           />
         ))}
+        {isLoading && <span>여기에 로딩 이미지 들어감다</span>}
       </S.ListContainer>
     </S.Container>
   );
